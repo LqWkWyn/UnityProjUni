815b6f32ee744d1d9b9e4599478c001aee0053fa
LqWkWyn <laurence.quinn.work@gmail.com>
2024-10-17 22:03:11 +0100

    FIrst cmt
    first commit

---
+++ week2/Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cd = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cd <=0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cd = 1;
+        }
+        if(cd >=0){
+            cd -= Time.deltaTime;
+        }
+        
+    }
+}
